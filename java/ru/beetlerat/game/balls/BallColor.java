ca875e325cf9194e0a2a7de1d8388c18ced87e6c
BeetleRat <86663719+BeetleRat@users.noreply.github.com>
2021-12-08 06:58:01 +0300

    Рефакторинг

--- java/ru/beetlerat/game/balls/BallColor.java
+++ java/ru/beetlerat/game/balls/BallColor.java
@@ -8,6 +8,35 @@ import java.io.IOException;
 
 // Класс
 public abstract class BallColor extends BallMovement {
+    // Константы типов шаров
+    public static final int YELLOW_BALL = 5;
+    public static final int PINK_BALL = 4;
+    public static final int GREEN_BALL = 3;
+    public static final int ORANGE_BALL = 2;
+    public static final int BLUE_BALL = 1;
+    public static final int BLACK_BALL = 0;
+
+    // Фабричный метод создания шара определенного типа
+    public static BallMovement createBall(int ballType, BallsGrid anotherBallsGrid, JComponent canvas, int row, int column, int id) {
+        switch (ballType) {
+            case BLACK_BALL:
+                return new BlackBall(anotherBallsGrid, canvas, row, column, id);
+            case BLUE_BALL:
+                return new BlueBall(anotherBallsGrid, canvas, row, column, id);
+            case ORANGE_BALL:
+                return new OrangeBall(anotherBallsGrid, canvas, row, column, id);
+            case GREEN_BALL:
+                return new GreenBall(anotherBallsGrid, canvas, row, column, id);
+            case PINK_BALL:
+                return new PinkBall(anotherBallsGrid, canvas, row, column, id);
+            case YELLOW_BALL:
+                return new YellowBall(anotherBallsGrid, canvas, row, column, id);
+            default:
+                System.out.println("Не существует шара типа: " + ballType);
+                return null;
+        }
+    }
+
     // Цвет шара
     protected Color color;
     // Объекты изображений
--- java/ru/beetlerat/game/balls/BallMovement.java
+++ java/ru/beetlerat/game/balls/BallMovement.java
@@ -9,35 +9,6 @@ import java.awt.event.MouseEvent;
 // выполняющийся в паралельном потоке
 // реализующий перемещение шра по игровому полю
 abstract class BallMovement implements Runnable {
-    // Константы типов шаров
-    public static final int YELLOW_BALL = 5;
-    public static final int PINK_BALL = 4;
-    public static final int GREEN_BALL = 3;
-    public static final int ORANGE_BALL = 2;
-    public static final int BLUE_BALL = 1;
-    public static final int BLACK_BALL = 0;
-
-    // Фабричный метод создания шара определенного типа
-    public static BallMovement createBall(int ballType, BallsGrid anotherBallsGrid, JComponent canvas, int row, int column, int id) {
-        switch (ballType) {
-            case BLACK_BALL:
-                return new BlackBall(anotherBallsGrid, canvas, row, column, id);
-            case BLUE_BALL:
-                return new BlueBall(anotherBallsGrid, canvas, row, column, id);
-            case ORANGE_BALL:
-                return new OrangeBall(anotherBallsGrid, canvas, row, column, id);
-            case GREEN_BALL:
-                return new GreenBall(anotherBallsGrid, canvas, row, column, id);
-            case PINK_BALL:
-                return new PinkBall(anotherBallsGrid, canvas, row, column, id);
-            case YELLOW_BALL:
-                return new YellowBall(anotherBallsGrid, canvas, row, column, id);
-            default:
-                System.out.println("Не существует шара типа: " + ballType);
-                return null;
-        }
-    }
-
 
     // Объект потока
     private final Thread thread;
